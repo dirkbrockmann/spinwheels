82ff92e0aba4c5e26d759291966c724346228fa6
Dirk Brockmannn <brockmann.dirk@gmail.com>
2023-02-02 20:35:49 +0100

    changes name to explorables/...

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
+const name = meta.name.substring(meta.name.lastIndexOf('/') + 1)
+
 module.exports = {
   entry: {
 	  index: './src/index.js',
@@ -13,18 +15,18 @@ module.exports = {
       new HtmlWebpackPlugin({
 		inject:'head' ,
         title: meta.title,
-		library: meta.name,
+		library: name,
   		template: './src/index.html',
-  		anchor: meta.name+"_container",
+  		anchor: name+"_container",
     	  description: meta.description,
 		scriptLoading: 'blocking'
       }),
-	  new BundleAnalyzerPlugin()
+	  //new BundleAnalyzerPlugin()
     ],
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'index.js',
-  	  library: meta.name,
+  	  library: name,
       clean: true 
     }, 
   module: {
