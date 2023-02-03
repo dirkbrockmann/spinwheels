08df69712940516abd4854b146ed0f2b600fe0e3
Dirk Brockmannn <brockmann.dirk@gmail.com>
2023-02-03 23:50:25 +0100

    changed default coupling to zero 3

--- src/parameters.js
+++ src/parameters.js
@@ -4,11 +4,11 @@ export default {
 		pinwheel_threshold:0.05,
 		oscillator_speed: {
 			range:[0,2],
-			default:0
+		default:1
 		},
 		coupling_strength: {
 			range:[0,1],
-			default:0.5
+			default:0
 		},
 		show_pinwheels: {
 			default: false
